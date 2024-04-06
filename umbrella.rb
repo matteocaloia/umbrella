e8160d52c5676362f47500b9f200277d38350e3f
matteocaloia <164690390+matteocaloia@users.noreply.github.com>
2024-04-06 19:12:35 +0000

    Geocoding part working

--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,47 @@
 pp "howdy"
+
+pp "Where are you located?"
+
+user_location = gets.chomp.gsub(" ", "%20")
+
+# user_location = "Chicago"
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = pp first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat)")
+pp longitude = loc.fetch("lng")
+
+
+
+
+
+
+
+
+
+pp results.class ClassName
+  
+end
 require "http"
 
 # Hidden variables
@@ -19,4 +62,3 @@ currently_hash = parsed_response.fetch("currently")
 current_temp = currently_hash.fetch("temperature")
 
 puts "The current temperature is " + current_temp.to_s + "."
-
